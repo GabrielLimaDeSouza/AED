f44f9708f731574553dcb35721fb39a450333608
Gabriel Lima de Souza <gabriel.souza.1354648@sga.pucminas.br>
2021-11-02 14:52:45 -0300

    Adição da sexta lista de exercícios

---
+++ Lista de Exercícios/Lista 5/Exercício 4/main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/* Calcule o resultado da seguinte série para os N primeiros termos,
+o valor de N deverá ser lido pelo teclado:
+S = 1/2 + 1/4 - 2/6 + 3/8 - 5/10 + 8/12 - 13/14 + 21/16 - ...
+Faça esse exercício das seguintes formas:
+4. utilizando procedimento passando N como parâmetro e
+retornando o valor de S em um outro parâmetro */
+
+void calculaS(int, float *);
+
+int main()
+{
+    setlocale(LC_ALL,"portuguese");
+    int n;
+    float S=0.5;
+    printf("Digite a quantidade de termos desejados: ");
+    scanf("%i",&n);
+    calculaS(n,&S);
+    printf("\nO valor de S é: %.4f\n",S);
+    return 0;
+}
+
+void calculaS(int n, float *resultado)
+{
+    int sinal=1,i,numerador=0,denominador=4,n2 = 1,n1 = 1;
+    float fracao;
+    for (i=0;i<n-1;i++)
+    {
+        /* Como o numerador é a série de Fibonacci,
+           essa parte do loop calcula a série*/
+        if (i==0)
+        {
+            numerador = 1;
+        }
+        else
+        {
+            numerador = n1+n2;
+            n1 = n2;
+            n2 = numerador;
+        }
+        // Fim do cálculo do numerador (Termo de Fibonacci)
+        // Início do cálculo do S, série pedida no exercício
+        fracao = (float)numerador/denominador;
+        *resultado = (float)(*resultado + fracao*sinal);
+        denominador = denominador + 2;
+        sinal = sinal * -1;
+        // Fim do cálculo do S
+    }
+    /*
+        Entrada           Saída Esperada       Saída do Programa        Observação
+           1                   0,5                   0,5                    OK
+           2                  0,75                   0,75                   OK
+           3                  0,41                   0,41                   OK
+           4                  0,79                   0,79                   OK
+           5                  0,29                   0,29                   OK
+           6                  0,95                   0,95                   OK
+    */
+}
